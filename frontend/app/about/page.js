275ef8d2a493500f84cf924777851e516dfc918c
Aditya <rajaditya3759@gmail.com>
2023-05-08 19:06:02 +0530

    about page is made

--- frontend/app/about/page.js
+++ frontend/app/about/page.js
@@ -1,7 +1,30 @@
 import React from "react";
 
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div className="w-[90%] mx-auto py-10 text-center">
+      <h2 className="text-2xl font-semibold">MonkeyNFT About Page</h2>
+      <p className="py-4 w-[70%] mx-auto text-lg text-left">
+        Welcome to the official about page for the MonkeyNFT collection! We are
+        a collection of 2000 unique monkey NFTs, each with its own distinctive
+        characteristics and traits. Our goal is to create a fun and engaging
+        experience for NFT collectors and enthusiasts. With our unique and
+        one-of-a-kind monkey NFTs, we hope to offer a unique investment
+        opportunity that also doubles as a piece of art. <br />
+        <br /> Our NFTs are created using cutting-edge blockchain technology,
+        ensuring that they are unique, verifiable, and secure. We believe that
+        the future of digital art and collectibles is in blockchain-based NFTs,
+        and we're excited to be at the forefront of this movement. Our team is
+        passionate about both art and technology, and we've worked hard to
+        create a collection that is both visually stunning and technically
+        advanced. We're committed to ensuring that our NFT holders have a
+        positive experience, and we're always here to answer any questions or
+        address any concerns you may have. Thank you for your interest in our
+        collection. We hope you'll join us on this exciting journey as we
+        continue to create and innovate in the world of NFT.
+      </p>
+    </div>
+  );
 };
 
 export default AboutPage;
